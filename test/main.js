865f23413301b779b8aad971e2763e57869bbf03
Ethan Rutherford <ethanrutherford2@gmail.com>
2017-10-07 18:47:16 -0700

    Improve the API

--- test/main.js
+++ test/main.js
@@ -4,9 +4,10 @@ const {
 	ImageLoader,
 	rgba,
 	builtIn: {
+		Shape,
 		OrthoCamera,
-		VectorShape,
-		SpriteShape,
+		VectorMaterial,
+		SpriteMaterial,
 	},
 } = require("../lib/2d-gl.js");
 
@@ -17,15 +18,18 @@ document.body.append(canvas);
 canvas.style.width = "100%";
 canvas.style.height = "100%";
 
-const triangleShape = new VectorShape(
+const scene = new Scene({bgColor: rgba(0, 0, 0, 1)});
+const camera = new OrthoCamera(0, 0, 10);
+
+const triangleShape = new Shape(
 	[{x: 0, y: 1}, {x: 0, y: 0}, {x: 1, y: 0}],
-	[rgba(1, 0, 0), rgba(0, 1, 0), rgba(0, 0, 1)],
 );
 
-const triangle = renderer.getInstance(triangleShape);
+const triangle = renderer.getInstance(triangleShape, new VectorMaterial(
+	[rgba(1, 0, 0), rgba(0, 1, 0), rgba(0, 0, 1)],
+));
 
-const scene = new Scene({renderables: [triangle], bgColor: rgba(0, 0, 0, 1)});
-const camera = new OrthoCamera(0, 0, 10);
+scene.add(triangle);
 
 function render() {
 	window.requestAnimationFrame(render);
@@ -37,16 +41,22 @@ render();
 
 const loader = new ImageLoader();
 loader.get("crate", "./crate.png").then((result) => {
-	const crateShape = new SpriteShape(
+	const crateShape = new Shape(
 		[{x: 0, y: 1}, {x: 0, y: 0}, {x: 1, y: 0}, {x: 1, y: 1}],
+	);
+	const crateMaterial = new SpriteMaterial(
 		[{x: 0, y: 1}, {x: 0, y: 0}, {x: 1, y: 0}, {x: 1, y: 1}],
 		result,
 	);
 
-	const crate = renderer.getInstance(crateShape);
-	crate.x = 2;
+	for (let i = 0; i < 10; i++) {
+		const crate = renderer.getInstance(crateShape, crateMaterial);
 
-	scene.add(crate);
+		crate.x = 2 + i / 4;
+		crate.y = i / 8 * (i % 2 ? 1 : -1);
+
+		scene.add(crate);
+	}
 });
 
 loader.onProgress = (progress) => {
