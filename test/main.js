921e09441ed02539811b0ae1158237d554563465
Ethan Rutherford <ethanrutherford2@gmail.com>
2018-02-21 23:05:35 -0800

    WIP post processing steps

--- test/main.js
+++ test/main.js
@@ -9,6 +9,9 @@ const {
 		VectorMaterial,
 		SpriteMaterial,
 	},
+	shaders: {
+		MotionBlur,
+	},
 } = require("../lib/2d-gl.js");
 
 const renderer = new Renderer();
@@ -21,6 +24,9 @@ canvas.style.height = "100%";
 const scene = new Scene({bgColor: rgba(0, 0, 0, 1)});
 const camera = new OrthoCamera(0, 0, 10);
 
+const blurShader = renderer.createShader(MotionBlur);
+scene.addPostProcShader(blurShader);
+
 const triangleShape = new Shape(
 	[{x: 0, y: 1}, {x: 0, y: 0}, {x: 1, y: 0}],
 );
@@ -31,18 +37,28 @@ const triangle = renderer.getInstance(triangleShape, new VectorMaterial(
 
 scene.add(triangle);
 
-function render() {
+let aCrate;
+
+function render(stamp) {
 	window.requestAnimationFrame(render);
-	triangle.r += .01;
+	const time = stamp / 120;
+
+	if (aCrate) {
+		const sin = Math.sin(time);
+		aCrate.x = sin * 5;
+		aCrate.r = -sin;
+	}
+
+	triangle.r = time;
 	renderer.render(camera, scene);
 }
 
-render();
+render(performance.now());
 
 const loader = new ImageLoader();
 loader.get("crate", "./crate.png").then((result) => {
 	const crateShape = new Shape(
-		[{x: 0, y: 1}, {x: 0, y: 0}, {x: 1, y: 0}, {x: 1, y: 1}],
+		[{x: -.5, y: .5}, {x: -.5, y: -.5}, {x: .5, y: -.5}, {x: .5, y: .5}],
 	);
 	const crateMaterial = new SpriteMaterial(
 		[{x: 0, y: 1}, {x: 0, y: 0}, {x: 1, y: 0}, {x: 1, y: 1}],
@@ -57,8 +73,37 @@ loader.get("crate", "./crate.png").then((result) => {
 
 		scene.add(crate);
 	}
+
+	aCrate = renderer.getInstance(crateShape, crateMaterial);
+	aCrate.x = 0;
+	aCrate.y = 3;
+	scene.add(aCrate);
 });
 
 loader.onProgress = (progress) => {
 	console.log(`${progress * 100}% loaded`);
 };
+
+//toggle blur shader
+let blurOn = true;
+window.addEventListener("keydown", (event) => {
+	if (event.key === "d") {
+		if (blurOn) {
+			scene.removePostProcShader(blurShader);
+		} else {
+			scene.addPostProcShader(blurShader);
+		}
+
+		blurOn = !blurOn;
+	}
+});
+
+window.addEventListener("touchend", () => {
+	if (blurOn) {
+		scene.removePostProcShader(blurShader);
+	} else {
+		scene.addPostProcShader(blurShader);
+	}
+
+	blurOn = !blurOn;
+});
